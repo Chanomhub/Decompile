0c7f738ea55493bab384ef064a103a4e18534c61
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-04-12 18:22:37 +0700

    Delete scr/Decrypter directory

--- scr/Decrypter/target/debug/deps/Decrypter-f88fefe2daae0e7b.d
+++
@@ -1,5 +0,0 @@
-/home/admi/Desktop/Decrypter/scr/Decrypter/target/debug/deps/Decrypter-f88fefe2daae0e7b: src/main.rs
-
-/home/admi/Desktop/Decrypter/scr/Decrypter/target/debug/deps/Decrypter-f88fefe2daae0e7b.d: src/main.rs
-
-src/main.rs:
